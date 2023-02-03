c69b6a7c36e84680d9d6a1369d50bdb81d37e91e
Yayroos <yayroos@protonmail.com>
2023-02-03 19:52:00 +1100

    tumblr de-tracker

---
+++ README.md
@@ -0,0 +1,5 @@
+A single function that takes a tracking URL for tumblr and fetches a non tracking version of that link
+
+Yayroos - 2023
+
+MIT lisence
---
+++ unTrack.js
@@ -0,0 +1,21 @@
+import('node-fetch');
+
+
+function unTrack(url){
+    fetch(url)
+    .then(response => {
+        //console.log(response);
+        return response.text();
+    })
+    .then(body => {
+        //console.log(body);
+        let url = body.match(/"(https:\/\/www\.tumblr\.com\/.*?)"/g);
+        url = url[0];
+        url = url.substring(1, url.indexOf('?'));
+        console.log(url);
+        let [ https, blank, tumblrcom, username, postID, postText] = url.split("/");
+        //[0]https: [1] [2]www.tumblr.com [3]username [4]postID [5]postText
+        let newURL = https + "//" + username + ".tumblr.com/" + postID + "/" + postText;
+        console.log(newURL);
+    });
+}
